151b6bda01e7223267574b3b4a0c26de4df83751
happysmile <812024380@qq.com>
2023-05-07 22:44:48 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Inspect from 'vite-plugin-inspect'
-import reducePluginSystem from './src/main.js'
+import reducePluginSystem from 'vite-plugin-reduce-plugin-system'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
